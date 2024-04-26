6036f4a6e4e080c67bce2e2802c5a7d5064a3e45
jaydev manik <jaydevmanik77@gmail.com>
2024-04-26 11:00:02 +0530

    AB#000000 added skeleton

---
+++ src/programs/test_tube_game.py
@@ -0,0 +1,21 @@
+class TestTubeGame:
+    test_tube_count = 0
+    soln_moves = []
+
+    def play(self):
+        self.get_number_of_testtubes()
+        self.get_colors_in_testtubes()
+        self.solve()
+        self.display_soln_moves()
+
+    def get_number_of_testtubes(self):
+        pass
+
+    def get_colors_in_testtubes(self):
+        pass
+
+    def solve(self):
+        pass
+
+    def display_soln_moves(self):
+        pass
